59babd3a394ccee62bd074bfbb9656c7cb6420a9
Charles Davis <charles.davis@borngroup.com>
2018-04-27 22:14:31 -0400

    parsed out files to modules

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,10 @@
+import Constants from 'modules/constants';
+import Dom from 'modules/dom';
+import Raf from 'modules/raf';
+import Util from 'modules/util';
+import Game from 'modules/game';
+import Render from 'modules/render';
+
 document.addEventListener('ready', function() {
     console.log('We are working!');
 });
